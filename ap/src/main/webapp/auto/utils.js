03e9f1d7a39016de9ca6b3fa96b3da1e8cace82d
opq4451 <opq4451@gmail.com>
2018-02-25 22:31:25 +0800

    更改下單機制

--- ap/src/main/webapp/auto/utils.js
+++ ap/src/main/webapp/auto/utils.js
@@ -44,3 +44,31 @@ function getRate(latter){
 	else 
 		return "9.618";
 }
+
+const betMinute = [4,5,6,9,10,11,14,15,16,19,20,21,24,25,26,29,30,31,34,35,36,39,40,41,44,45,46,49,50,51,54,55,56,59,0,1];
+const falseMinute = [2,7,12,17,22,27,32,37,42,47,52,57];
+
+const clearMinute = [6,11,16,21,26,31,36,41,46,51,56,1];
+function checkFalseMinute(m){ 
+	for(var i in falseMinute){
+		if(falseMinute[i] == m)
+			return true;
+	}
+	return false
+}
+
+function checkBetMinute(m){ 
+	for(var i in betMinute){
+		if(betMinute[i] == m)
+			return true;
+	}
+	return false
+}
+
+function checkClearMinute(m){ 
+	for(var i in clearMinute){
+		if(clearMinute[i] == m)
+			return true;
+	}
+	return false
+}
